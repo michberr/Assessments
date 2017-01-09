a929b8a397b67f2c851f4645ca7b8fc64d661708
Michelle Berry <michberr8@gmail.com>
2017-01-08 19:25:03 -0800

    adding list assessment

---
+++ lists/assessment.py
@@ -0,0 +1,162 @@
+"""List Assessment
+
+Edit the functions until all of the doctests pass when
+you run this file.
+"""
+
+
+def all_odd(numbers):
+    """Return a list of only the odd numbers in the input list.
+
+    For example::
+
+        >>> all_odd([1, 2, 7, -5])
+        [1, 7, -5]
+
+        >>> all_odd([2, -6, 8])
+        []
+    """
+
+    # If list contains non-integers, print error message,
+    # else, return odd numbers.
+    if not all(isinstance(num, int) for num in numbers):
+        print "Non-integer input"
+    else:
+        return [num for num in numbers if num % 2 != 0]
+
+
+def print_indices(items):
+    """Print index of each item in list, followed by item itself.
+
+    Do this without using a "counting variable" --- that is, don't
+    do something like this::
+
+        count = 0
+        for item in list:
+            print count
+            count = count + 1
+
+    Output should look like this::
+
+        >>> print_indices(["Toyota", "Jeep", "Volvo"])
+        0 Toyota
+        1 Jeep
+        2 Volvo
+
+        >>> print_indices(["Toyota", "Jeep", "Toyota", "Volvo"])
+        0 Toyota
+        1 Jeep
+        2 Toyota
+        3 Volvo
+
+    """
+    for i in range(len(items)):
+        print '{} {}'.format(i, items[i])
+
+
+
+def foods_in_common(foods1, foods2):
+    """Find foods in common.
+
+    Given 2 lists of foods, return the items that are in common
+    between the two, sorted alphabetically.
+
+    **NOTE**: for this problem, you're welcome to use any of the
+    Python data structures you've been introduced to (not just
+    lists). Is there another that would be a good idea?
+
+    For example::
+
+        >>> foods_in_common(
+        ...     ["cheese", "bagel", "cake", "kale", "zebra cakes"],
+        ...     ["hummus", "cheese", "beets", "kale", "lentils", "bagel", "cake" ]
+        ... )
+        ['bagel', 'cake', 'cheese', 'kale']
+
+    If there are no foods in common, return an empty list::
+
+        >>> foods_in_common(
+        ...     ["lamb", "chili", "cheese"],
+        ...     ["cake", "ice cream"]
+        ... )
+        []
+
+    """
+
+    food_intersect = [food for food in foods1 if food in foods2]
+    return sorted(food_intersect)
+
+def foods_in_common2(foods1, foods2):
+    """ Find foods in common.
+
+    This function returns identical output to foods_in_common, but is
+    implemented with a set data structure instead of a list.
+
+    I benchmarked both functions with the example provided above.
+    foods_in_common took 1.19 E-5 to run and foods_in_common2 took 3.22 E-5
+    to run. Therefore,with this example, the list version is actually faster,
+    perhaps because of the time it takes to convert between data types.
+    These results might change with a longer list.
+
+    """
+
+    return sorted(set(foods1).intersection(foods2))
+
+
+def every_other_item(items):
+    """Return every other item in `items`, starting at first item.
+
+    For example::
+
+       >>> every_other_item([1, 2, 3, 4, 5, 6])
+       [1, 3, 5]
+
+       >>> every_other_item(
+       ...   ["you", "z", "are", "z", "good", "z", "at", "x", "code"]
+       ... )
+       ['you', 'are', 'good', 'at', 'code']
+    """
+
+    return items[0::2]
+
+
+def largest_n_items(items, n):
+    """Return the `n` largest integers in list, in ascending order.
+
+    You can assume that `n` will be less than the length of the list.
+
+    For example::
+
+        >>> largest_n_items([2, 6006, 700, 42, 6, 59], 3)
+        [59, 700, 6006]
+
+    It should work when `n` is 0::
+
+        >>> largest_n_items([3, 4, 5], 0)
+        []
+
+    If there are duplicates in the list, they should be counted
+    separately::
+
+        >>> largest_n_items([3, 3, 3, 2, 1], 2)
+        [3, 3]
+    """
+
+    # Sort the list in descending order
+    items.sort(reverse=True)
+
+    # Return a slice up to the nth largest number, and reverse the sorting
+    return sorted(items[0:n])
+
+
+#####################################################################
+# END OF ASSESSMENT: You can ignore everything below.
+
+if __name__ == "__main__":
+    import doctest
+
+    print
+    result = doctest.testmod()
+    if not result.failed:
+        print "ALL TESTS PASSED. GOOD WORK!"
+    print
